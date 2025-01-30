b5165f968c7b11b7d8b53b974848226601d0e0de
weltherjoe <weltherjoe@yahoo.com.br>
2025-01-30 11:45:27 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,8 @@ import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-bool isDarkMode = false;
 Color backgroundColor = Colors.white;
 Color textColor = Colors.black;
-const Color backgroundColorLight = Colors.white;
-const Color textColorLight = Colors.black;
-const Color backgroundColorDark = Colors.black;
-const Color textColorDark = Colors.white;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,27 +19,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -56,15 +35,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -74,21 +44,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
@@ -148,19 +107,30 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     FilledButton(
-                        onPressed: () => setState(() {
-                              isDarkMode = true;
-                              textColor = textColorDark;
-                              backgroundColor = backgroundColorDark;
-                            }),
-                        child: const Text("Dark Mode")),
+                      onPressed: () => setState(() {
+                        backgroundColor = Colors.black;
+                      }),
+                      child: const Text("Black Background"),
+                    ),
                     FilledButton(
                         onPressed: () => setState(() {
-                              isDarkMode = false;
-                              textColor = textColorLight;
-                              backgroundColor = backgroundColorLight;
+                              backgroundColor = Colors.white;
                             }),
-                        child: const Text("Light Mode")),
+                        child: const Text("White Background")),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    FilledButton(
+                      onPressed: () =>
+                          setState(() => backgroundColor = Colors.purple),
+                      child: const Text("Purple Background"),
+                    ),
+                    FilledButton(
+                        onPressed: () =>
+                            setState(() => backgroundColor = Colors.amber),
+                        child: const Text("Amber Background")),
                   ],
                 ),
                 Row(
